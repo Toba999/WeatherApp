95f8c911d7beb46c16bd836816493cd6ed4fcbe8
Toba <mohamedtowba298@gmail.com>
2022-05-03 18:07:51 +0200

    adding resources

--- app/src/main/java/com/example/weatherapp/splashScreen/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/splashScreen/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.weatherapp
+package com.example.weatherapp.splashScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
